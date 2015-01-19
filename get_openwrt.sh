c00c36307ab8519fcc6662b73651360601ec69c9
Marc Juul <juul@sudomesh.org>
2015-01-18 20:23:05 -0800

    get_openwrt.sh was lying about openwrt version

--- get_openwrt.sh
+++ get_openwrt.sh
@@ -3,8 +3,8 @@
 echo "Creating tftp_root directory"
 mkdir -p tftp_root
 cd tftp_root
-echo "Downloading OpenWRT Attitude Adjustment squashfs root image"
+echo "Downloading OpenWRT Barrier Breaker squashfs root image"
 wget "http://downloads.openwrt.org/barrier_breaker/14.07/atheros/generic/openwrt-atheros-root.squashfs" &&
-echo "Downloading OpenWRT Attitude Adjustment vmlinux image" &&
+echo "Downloading OpenWRT Barrier Breaker vmlinux image" &&
 wget "http://downloads.openwrt.org/barrier_breaker/14.07/atheros/generic/openwrt-atheros-vmlinux.gz" &&
 echo "Download complete"
